3bb2186da23555fa73e33616b0730116ace526dc
filipesantanadev <filipe.santana@al.infnet.edu.br>
2026-05-08 22:50:16 -0300

    feat: implementar caso de uso de autenticação com validação e tratamento de erros

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,38 @@
+import type { UsersRepository } from '@/repositories/users-repository'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import type { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const isPasswordValid = await compare(password, user.passwordHash)
+
+    if (!isPasswordValid) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
