553693d468159687ba32db4e4f525d95d295948f
filipesantanadev <filipe.santana@al.infnet.edu.br>
2026-05-10 00:29:07 -0300

    feat: implementar o caso de uso GetUserProfile com tratamento de erros, métodos de repositório e teste unitário

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,36 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      passwordHash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({ userId: createUser.id })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'non-existing-user-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
